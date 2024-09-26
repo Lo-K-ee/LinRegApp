b53f4cbdedac27c1b23af338739df80be36c811a
Lokesh Palaniraj <130686597+Lo-K-ee@users.noreply.github.com>
2024-09-26 17:44:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,9 @@ file = col3.file_uploader('Upload your CSV file', type="csv")
 
 with st.expander('Uploaded raw data'):
   if file is not None: 
-  df = pd.read_csv(file)
-  st.write('The file has been successfully uploaded:')
-  df
+    df = pd.read_csv(file)
+    st.write('The file has been successfully uploaded:')
+    df
 
 # with st.expander('Select the variables for further analysis'):
 #   col_name = df.columns.to_list()
