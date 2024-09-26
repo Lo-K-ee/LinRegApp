8df65f6412d1462a313ce344f6c0a909087a8b26
Lokesh Palaniraj <130686597+Lo-K-ee@users.noreply.github.com>
2024-09-26 18:14:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ with st.expander('Uploaded raw data'):
 if df is not None:
   with st.expander('Select the variables for further analysis'):
     col_name = df.columns.to_list()
-    dependant = st.selectbox('Choose the dependant variables:', col_name)
+    dependant = st.multiselect('Choose the dependant variables:', col_name)
     
 
 
