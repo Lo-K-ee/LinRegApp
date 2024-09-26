603dade6e68d722219b89e95a60ae65c8b4d6af2
Lokesh Palaniraj <130686597+Lo-K-ee@users.noreply.github.com>
2024-09-26 14:48:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,7 @@ st.info('Hey, Welcome!!!')
 st.write('This is a linear regression app that deploys the regression model for your data based on your inputs, please upload your data to proceed:')
 
 
-col1, col2, col3 = st.beta_columns([1,1,1])
-col2.title("Centered! :)")
-# col2.button('upload')
+col1, col2, col3 = st.columns([1,1,1])
+col2.button('upload')
 
 
