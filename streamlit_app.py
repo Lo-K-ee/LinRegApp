025e63c1fc85cbfe6588e9a86dba0f3cfec6ffab
Lokesh Palaniraj <130686597+Lo-K-ee@users.noreply.github.com>
2024-09-26 15:05:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,8 @@ st.write('This is a linear regression app that deploys the regression model for 
 st.text("") #add-line
 col1, col2, col3, col4, col5 = st.columns([1,1,1,1,1]) #splitting the space into 5 columns for alignment
 st.markdown("***")
-# with col3.button('upload your csv file'):
-col3.file_uploader('upload your csv file', type="csv")
-  # df = st.file_uploader('upload your csv file', type="csv")
+df = col3.file_uploader('Upload your CSV file', type="csv", help)
+
 
 
 
