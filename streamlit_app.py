cc4caaad5cbccdf946f519cd0aa5e25da0ff82ed
Lokesh Palaniraj <130686597+Lo-K-ee@users.noreply.github.com>
2024-09-26 14:51:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,10 @@ st.info('Hey, Welcome!!!')
 
 st.write('This is a linear regression app that deploys the regression model for your data based on your inputs, please upload your data to proceed:')
 
-st.text("")
-col1, col2, col3, col4, col5 = st.columns([1,1,1,1,1])
+st.text("") #add-line
+col1, col2, col3 = st.columns([1,1,1]) #splitting the space into 3 columns for alignment
 st.markdown("***")
-col3.button('upload')
+col2.button('upload')
 
 
 
