c5d0de72976a8a0c995bce6c0623619382655446
Lokesh Palaniraj <130686597+Lo-K-ee@users.noreply.github.com>
2024-09-26 14:40:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('📈 Linear Regression App')
 
-st.write('Hello world!')
+st.info('Hey, Welcome!!!')
+
+st.write('This is a linear regression app that deploys the regression model for your data based on your inputs, please upload your data to proceed:')
+
+st.button(upload)
+
