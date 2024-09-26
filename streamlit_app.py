36af942c773fc87a5d78b42c66edc4c44cde0623
Lokesh Palaniraj <130686597+Lo-K-ee@users.noreply.github.com>
2024-09-26 17:48:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.text("") #add-line
 col1, col2, col3, col4, col5 = st.columns([1,1,1,1,1]) #splitting the space into 5 columns for alignment
 st.markdown("***")
 file = col3.file_uploader('Upload your CSV file', type="csv")
-
+df = None
 
 with st.expander('Uploaded raw data'):
   if file is not None: 
@@ -19,10 +19,10 @@ with st.expander('Uploaded raw data'):
     st.write('The file has been successfully uploaded:')
     df
 
-# with st.expander('Select the variables for further analysis'):
-#   col_name = df.columns.to_list()
-#   # st.write('Choose the dependant variables:')
-#   st.radio('Choose the dependant variables:', col_name)
+with st.expander('Select the variables for further analysis'):
+  col_name = df.columns.to_list()
+  # st.write('Choose the dependant variables:')
+  st.radio('Choose the dependant variables:', col_name)
 
 
 
